aebb65d34646d5ac0c202798400396643853a067
TheDemonLord333 <thedemonlord333@gmail.com>
2026-06-25 23:54:55 +0200

    Initial Commit

---
+++ DemonicCalc333/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemonicCalc333
+//
+//  Created by David Martens on 25.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemonicCalc333/DemonicCalc333App.swift
@@ -0,0 +1,17 @@
+//
+//  DemonicCalc333App.swift
+//  DemonicCalc333
+//
+//  Created by David Martens on 25.06.26.
+//
+
+import SwiftUI
+
+@main
+struct DemonicCalc333App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
